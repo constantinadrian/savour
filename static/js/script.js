73deab7dd5bc8d363acf739077d6309bdaeadbaa
constantinadrian <adrianc.chiriac@yahoo.com>
2021-05-10 09:05:43 +0000

    [Add] Added shop section on landing page

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // Change the recipe title display if lenght is
-$(".card-recipe-title").each(function() {
+$(".js-card-title").each(function() {
     let cardRecipeTitle = this.textContent
     if (cardRecipeTitle.length > 57) {
         cardRecipeTitle = cardRecipeTitle.substring(0,53) + "..."
