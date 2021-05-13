533ddbae6eac188de27f49a74715a5f3a0e6d8cf
constantinadrian <adrianc.chiriac@yahoo.com>
2021-05-13 01:12:37 +0000

    [Add] Added functionality for steps fields

--- static/js/add_recipe.js
+++ static/js/add_recipe.js
@@ -1,3 +1,6 @@
+// Get all textarea fields from the page
+let textarea = $("textarea");
+
 /**
  * Add ingredients fields for recipe
  */
@@ -5,7 +8,7 @@ function addIngredients() {
     let ingredietsList = document.querySelector(".ingredients-list");
     let ingredients = document.querySelectorAll(".ingredient");
 
-
+    // show remove btn once user has requested new field
     if (ingredients.length == 1) {
         $(".btn-remove-ingredients").css("display", "inline-block");
     }
@@ -15,16 +18,12 @@ function addIngredients() {
         
         inputField = `
                         <input type="text" class="form-control" name="recipe-ingredient" required>
-                    `;   
+                     `;   
 
         listItem.setAttribute("class", "ingredient");
         listItem.innerHTML = inputField;
 
         ingredietsList.insertBefore(listItem, ingredietsList.lastChild.nextSibling);
-
-    }
-    else {
-        console.log("You reach maximum of ingredients fields");
     }
 }
 
@@ -46,6 +45,84 @@ function removeIngredients() {
     }
 }
 
+/**
+ * Add steps fields for recipe
+ */
+function addMethods() {
+    let methodsList = document.querySelector(".methods-list");
+    let methods = document.querySelectorAll(".methods");
+
+    // show remove btn once user has requested new field
+    if (methods.length == 1) {
+        $(".btn-remove-steps").css("display", "inline-block");
+    }
+
+    if (methods.length != 5) {
+        let stepItem = document.createElement('li');
+        
+        textareaField = `
+                            <textarea class="form-control" name="recipe-methods" rows="1" maxlength="320" required></textarea>
+                        `;   
+
+        stepItem.setAttribute("class", "methods");
+        stepItem.innerHTML = textareaField;
+
+        methodsList.insertBefore(stepItem, methodsList.lastChild.nextSibling);
+
+    }
+    else {
+        console.log("You reach maximum of steps fields");
+    }
+
+    // reasign the new numbers of fields
+    textarea = $("textarea");
+
+    resizeTextarea(textarea);
+}
+
+/**
+ * Remove step fields from recipe
+ */
+function removeMethods() {
+    let methodsList = document.querySelector(".methods-list");
+
+    // remove last child from methods list
+    methodsList.removeChild(methodsList.lastElementChild);
+
+    // get all the nodes of methods list
+    let methods = document.querySelectorAll(".methods");
+
+    // check if is more than one otherwise hide the remove btn
+    if (methods.length == 1) {
+        $(".btn-remove-steps").css("display", "none")
+    }
+}
+
+/**
+ * Resize textarea on user input
+ * @param {Object} textarea - All textarea fields from the page 
+ */
+function resizeTextarea(textarea) {
+    textarea.each(function (){}).on("input", function () {
+        $(this).css("height", "auto");
+        $(this).css("height", this.scrollHeight);
+    });
+}
+
+/**
+ * Resize textarea on window resize
+ * @param {Object} textarea - All textarea fields from the page 
+ */
+function windowResize(textarea) {
+    textarea.each(function (){
+        $(this).css("height", "auto");
+        $(this).css("height", this.scrollHeight);
+    });
+}
+
+// // Call the resize function 
+resizeTextarea(textarea);
+
 // Add more ingredients fields
 $(".btn-add-ingredients").click(function(){
     addIngredients()
@@ -56,3 +133,17 @@ $(".btn-remove-ingredients").click(function(){
     removeIngredients()
 });
 
+// Add more methods fields
+$(".btn-add-steps").click(function(){
+    addMethods()
+});
+
+// Remove methods fields
+$(".btn-remove-steps").click(function(){
+    removeMethods()
+});
+
+// Resize textarea on window resize
+$(window).resize(function() {
+    windowResize(textarea)
+});
