59d0f2267f780fa34392d65d88bf590c29c8e568
constantinadrian <adrianc.chiriac@yahoo.com>
2021-06-05 02:01:57 +0000

    [Change] Change the debug to False

--- app.py
+++ app.py
@@ -1089,4 +1089,4 @@ def error(code):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
